75c8f9e884ef0cae5f0ccbb6d17d8e0395407af7
Daniel Zych <danjzych@gmail.com>
2024-02-14 14:13:17 -0600

    Made 531.sql print more readable commands to the terminal. Added script to seed db to package.json

--- api/531.sql
+++ api/531.sql
@@ -1,26 +1,38 @@
 \echo 'Delete and recreate _531 database?'
+\echo '\n'
 \prompt 'Return for yes or ctrl-C to cancel > ' foo
 
 DROP DATABASE _531;
 CREATE DATABASE _531;
 \connect _531;
 
+\echo '\n'
 \echo 'Building schema...'
 \i 531-schema.sql
 
+\echo '\n'
 \echo 'Seeding data...'
 \i 531-seed.sql
 
+\echo '\n'
+\echo 'Done creating _531.'
 
+\echo '\n'
 \echo 'Delete and recreate _531_test database?'
+\echo '\n'
 \prompt 'Return for yes or ctrl-C to cancel > ' foo
 
 DROP DATABASE _531_test;
 CREATE DATABASE _531_test;
 \connect _531_test;
 
+\echo '\n'
 \echo 'Building schema...'
 \i 531-schema.sql
 
+\echo '\n'
 \echo 'Seeding data...'
 \i 531-seed.sql
+
+\echo '\n'
+\echo 'Done creating _531_test.'
