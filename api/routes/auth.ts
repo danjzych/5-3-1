1b3ded92c712c20b7ee4d496c0e5414c2740ddd7
Daniel Zych <danjzych@gmail.com>
2024-02-23 10:39:33 -0600

    Added schema for user registration route

--- api/routes/auth.ts
+++ api/routes/auth.ts
@@ -3,6 +3,7 @@ import User from "../models/User.js";
 import { createToken } from "../helpers/token.js";
 import * as jsonschema from "jsonschema";
 import authSchema from "../schemas/auth.json" assert { type: "json" };
+import registerSchema from "../schemas/register.json" assert { type: "json" };
 import { BadRequestError } from "../expressError.js";
 
 const router = express.Router({ mergeParams: true });
@@ -11,7 +12,7 @@ const router = express.Router({ mergeParams: true });
  *
  * Returns JWT token which can be used to authenticate further requests.
  *
- * Authorization required: none
+ * Auth: none
  */
 
 router.post("/token", async function (req, res, next) {
@@ -29,7 +30,22 @@ router.post("/token", async function (req, res, next) {
   return res.json({ token });
 });
 
+/** POST /auth/register: { username, password, email } => { User }
+ *
+ * Returns user object object.
+ *
+ * Auth: None
+ *
+ */
 router.post("/register", async function (req, res, next) {
+  const validator = jsonschema.validate(req.body, registerSchema, {
+    required: true,
+  });
+  if (!validator.valid) {
+    const errs = validator.errors.map((e) => e.stack).join(", ");
+    throw new BadRequestError(errs);
+  }
+
   const newUser = await User.register(
     req.body.username,
     req.body.password,
